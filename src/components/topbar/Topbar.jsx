7cd22b18aaf762ba0c57babed513a88eeda2bd0d
Daz <shoeshinesamurai@gmail.com>
2024-11-27 22:43:46 -0800

    MUI install

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Chat, Notifications, Search } from '@mui/icons-material';
 
 export default function Topbar() {
   return <div className='topbarContainer'>
@@ -7,6 +8,7 @@ export default function Topbar() {
     </div>
     <div className="topbarCenter">
       <div className="searchbar">
+        <Search className="searchIcon" />
         <input
           type="text"
           className='searchInput'
@@ -15,8 +17,14 @@ export default function Topbar() {
       </div>
     </div>
     <div className="topbarRight">
-      <div className="topbarIconItem">1</div>
-      <div className="topbarIconItem">2</div>
+      <div className="topbarIconItem">
+        <Chat />
+        <span className="topbarIconBadge">1</span>
+      </div>
+      <div className="topbarIconItem">
+        <Notifications />
+        <span className="topbarIconBadge">2</span>
+      </div>
     </div>
     <img src="/assets/person/1.jpeg" alt="" className='topbarImg' />
   </div>;
