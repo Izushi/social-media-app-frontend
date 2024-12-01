e1ba62010068f880fb4c9b00fe61586a3f56bf01
Daz <shoeshinesamurai@gmail.com>
2024-12-01 00:31:11 -0800

    Created components of home container

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
-import TopBar from '../../components/top_bar/TopBar'
+import React from 'react';
+import './Home.css';
+import TopBar from '../../components/top_bar/TopBar';
+import Sidebar from '../../components/sidebar/Sidebar';
+import Timeline from '../../components/timeline/Timeline';
+import RightBar from '../../components/right_bar/RightBar';
 
 export default function Home() {
   return (
     <>
       <TopBar />
-      {/* sidebar */}
-      {/* timeline */}
-      {/* rightbar */}
+      <div className="homeContainer">
+        <Sidebar />
+        <Timeline />
+        <RightBar />
+      </div>
     </>
   )
 }
