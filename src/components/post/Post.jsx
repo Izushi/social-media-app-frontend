1ff946ec5c155914e455d00e006388a32e3c43b8
Daz <shoeshinesamurai@gmail.com>
2024-12-17 00:38:18 -0800

    Created Post Component

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -7,12 +7,14 @@ export default function Post() {
     <div className='post'>
       <div className="postWrapper">
         <div className="postTop">
-          <img src="./assets/person/1.jpeg" alt="" className='postProfileImg' />
-          <span className="postUserName">Daz</span>
-          <span className="postDate">5 min ago</span>
-        </div>
-        <div className="postRight">
-          <MoreVert />
+          <div className="postTopLeft">
+            <img src="./assets/person/1.jpeg" alt="" className='postProfileImg' />
+            <span className="postUserName">Daz</span>
+            <span className="postDate">5 min ago</span>
+          </div>
+          <div className="postTopRight">
+            <MoreVert />
+          </div>
         </div>
         <div className="postCenter">
           <span className="postText">Creating SNS App</span>
