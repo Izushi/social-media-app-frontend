0c98e9364a91b9d690ac67e82e6d740429852297
Daz <shoeshinesamurai@gmail.com>
2024-12-19 00:57:27 -0800

    Created RightBar Component

--- src/components/right_bar/RightBar.jsx
+++ src/components/right_bar/RightBar.jsx
@@ -3,6 +3,26 @@ import './RightBar.css';
 
 export default function RightBar() {
   return (
-    <div className='rightBar'>RightBar</div>
+    <div className='rightBar'>
+      <div className="rightBarWrapper">
+        <div className="eventContainer">
+          <img src="assets/star.png" alt="" className='starImg' />
+          <span className="eventText">
+            <b>Now holding followers-only event</b>
+          </span>
+        </div>
+        <img src="assets/event.jpeg" alt="" className='eventImg' />
+        <h4 className='rightBarTitle'>Friends online</h4>
+        <ul className="rightBarFriendList">
+          <li className="rightBarFriend">
+            <div className="rightBarProfileImgContainer">
+              <img src="assets/person/1.jpeg" alt="" />
+              <span className="rightBarOnline"></span>
+            </div>
+            <span className="rightBarUsername">Daz</span>
+          </li>
+        </ul>
+      </div>
+    </div>
   )
 }
